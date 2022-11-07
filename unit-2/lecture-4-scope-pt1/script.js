de94c6764d1a066cd9e5a4818449e9c99e64ec44
gonzalomarcylabschool <gonzalo@marcylabschool.org>
2022-11-07 00:00:32 -0500

    updated the JS file for the lecture

--- unit-2/lecture-4-scope-pt1/script.js
+++ unit-2/lecture-4-scope-pt1/script.js
@@ -9,12 +9,63 @@
 //     //any variable that is declared here will have a local scope
 //     var c = "also this";// What Scope do you think this variable have?
 // }
-let  string=""
-for (let i = 0; i < 7; i++){
-    if (i%2 === 0){
-        string += "1";
-    }else{
-        string += "0";
-    }
-}
-console.log(string);
+
+// //var a = 1;
+// function my(){
+//     var a = 2; 
+//     console.log(a);
+// }
+// //var a = 3;
+// console.log(a);
+// my();
+// //var a = 4;
+// console.log(a);
+
+// var b = 1;
+// console.log(b);
+// if (true){
+//     var b = 2;
+//     console.log(b);
+// }
+// console.log(b);
+
+// let c  = 1;
+// console.log(c);
+// if (true){
+//     let c = 2;
+//     console.log(c);
+// }
+// console.log(c);
+
+
+// let x = 1;
+// console.log(x)
+// if (true){
+//     let x = 2;
+//     console.log(x);
+// }
+// console.log( x);
+// let x = 1; //global scope
+//   console.log(x)// will print 1
+//   if (true){
+//       let x = 2;// block scope
+//       let y = 3; // block scope
+//       console.log(x); //will print 2
+//       console.log(y); // will print 3
+//   }
+//   console.log( x);// will print 1
+//   //console.log(y); // will return a refrence error. 
+//   x = 4;
+//   console.log(x);
+
+// const z = 1;
+//   console.log(z)
+//   if (true){
+//     const z = 2;
+//     const w = 3;
+//     console.log(z);
+//     console.log(w);
+//   }
+//   //console.log(w);
+//   z = 4; // will return and error
+//   console.log(z);
