889aa6e6d42a998495f4d1a6eb496d271189f821
Gonzalomarcylabschool <Gonzalo@marcylabschool.org>
2022-11-09 18:26:49 +0000

    updated based on the lecture code

--- unit-2/lecture-6-array-pt2/script.js
+++ unit-2/lecture-6-array-pt2/script.js
@@ -1,13 +1,13 @@
 //loops
 
-const scores = [22, 54, 76, 92, 43, 33];
+const scores = [22, 54, 101, 92, 43, 33];
 //for loops
 // for (let i = 0; i < scores.length; i++) {//(counter; end point; by how much)
 //     console.log(scores[i]);//what we would like to happen to each element of the array.
 // }
 
 //while loops
-// let i = 0;//counter
+ let i = 0;//counter
 
 // while (i < scores.length) {//end point
 //     console.log(scores[i]); // what we would like to happen 
@@ -17,7 +17,7 @@ const scores = [22, 54, 76, 92, 43, 33];
 // do while loops
 // do {
 //     console.log(scores[i]);//what we would like to happen
-//     i++;//counter
+//     i++;//by how much
 // } while (i < scores.length);//end point
 
 
@@ -27,9 +27,10 @@ const scores = [22, 54, 76, 92, 43, 33];
 // }
 
 //for...Of loops
-// these are alittle diffrent 
-// for (par of scores) {//variable to store each element from the array and then passthough to the code.
+//these are alittle diffrent 
+// for (let par of scores) {//variable to store each element from the array and t
 //     console.log(par);
+    
 // }
 
 //forEach loops
@@ -51,10 +52,12 @@ const scores = [22, 54, 76, 92, 43, 33];
 
 
 // let nums;
-// const numArray = [1, 2, 3, 4, 5, 6]
+// const numArray = [1, 2, 3, 4, 5, 6, 3, 4, 5, 6, 3, 4, 5, 6, 3, 4, 5, 6, 3, 4, 5, 6]
 // nums = [...numArray];
-// console.log(numArray);
-// console.log(nums);
+// nums.push(100000);
+//  console.log(numArray);
+//  console.log(nums);
+
 
 // let baked_desserts = ['cake', 'cookie', 'donut'];
 // let desserts = ['icecream', 'flan', 'frozen yoghurt', ...baked_desserts];
@@ -63,12 +66,12 @@ const scores = [22, 54, 76, 92, 43, 33];
 // let desserts2 = ['icecream', 'flan', ...baked_desserts, 'frozen yoghurt'];
 // console.log(desserts2);
 
-// function sum(x, y, z) {
-//     return x + y + z;
-//   }
-// const numbers = [1, 2, 3];
+function sum(x, y, z, d) {
+    return x + y + z + d;
+  }
+const numbers = [75, 10, 12, 99, 9];
 
-// console.log(sum(...numbers));
+console.log(sum(...numbers));
 
 
 
