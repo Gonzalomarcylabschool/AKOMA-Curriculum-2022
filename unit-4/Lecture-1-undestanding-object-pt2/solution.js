75c7d2bdbc3aa101c0327237698fc89e92d8f755
Gonzalo Romero <111442707+Gonzalomarcylabschool@users.noreply.github.com>
2022-12-09 09:36:05 -0500

    Update solution.js

--- unit-4/Lecture-1-undestanding-object-pt2/solution.js
+++ unit-4/Lecture-1-undestanding-object-pt2/solution.js
@@ -7,18 +7,17 @@ function makeAmazonCart(userAccount) {
             return `${itemName} added to the cart`
         },
         removeItem(itemName) {
-            const indexToRemove = this.items.findIndex(element => element.itemName === itemName);
+            const indexToRemove = this.items.findIndex(item => item.itemName === itemName);
             if (indexToRemove >= 0) {
-                const before = this.items.slice(0, indexToRemove);
-                const after = this.items.slice(indexToRemove + 1);
-                this.items = [...before, ...after];
+                if(target !== -1){ 
+                this.contacts.splice(target,1)
             }
         },
         getTotal() {
             return this.items.reduce((total, item) => total + item.price, 0)
         },
         getItemList() {
-            return this.items.map(element => element.itemName)
+            return this.items.map(item => item.itemName)
         },
         removeMostExpensiveItem() {
             this.items.sort((a, b) => a.price < b.price ? -1 : 1)
@@ -29,15 +28,15 @@ function makeAmazonCart(userAccount) {
 
 const cart = makeAmazonCart("Gonzalo");
 console.log(cart);
-cart.addItem("", 1);
-cart.addItem("banana", 0.5);
-cart.addItem("cherries", 2.5);
-cart.addItem("dates", 3);
-cart.addItem("eggplant", 1.5);
+cart.addItem("Mechanical Keyboard", 26.99);
+cart.addItem("Large Mouse Pad", 10);
+cart.addItem("Gaming Mouse", 8.5);
+cart.addItem("Office chair", 160);
+cart.addItem("Clock", 10);
 
 console.log(cart.items);
 console.log(cart.getTotal());
 
-cart.removeItem("dates")
+cart.removeItem("Clock")
 cart.removeMostExpensiveItem()
 console.log(cart);
