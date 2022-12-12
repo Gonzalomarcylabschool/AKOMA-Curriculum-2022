d7b756244e42c2c0c246fcebf5ddebc30d498167
gonzalomarcylabschool <gonzalo@marcylabschool.org>
2022-12-12 12:02:33 -0500

    updated the code for the lecutre

--- unit-4/Lecture-2-Function-Execution-Context/script.js
+++ unit-4/Lecture-2-Function-Execution-Context/script.js
@@ -1,22 +1,47 @@
-debugger;
-var a = 'Gonzalo';
-var aa = 1;
-var aaa = 2;
-
-function myFunction() { // The whole function is stored.
-    console.log('this is a function'); // will not run until the function is invoked in the execution phase.
-}
-myFunction(); 
 
 
+// // //debugger;
+// var a = 'Gonzalo';
+// var aa = 1;
+// var aaa = 2;
+
+// // function myFunction() { // The whole function is stored.
+// //     console.log('this is a function'); // will not run until the function is invoked in the execution phase.
+// // }
+// // //myFunction(); 
+
+// debugger;
 // function add(num1, num2) { 
 //     console.log('this is a function'); 
+//     return num1 + num2
 // }
 // var aaaa = add(aa, aaa);
 // var aaaaa = add(5, 10);
 
-console.log(this)
+//console.log(this)
 
+//function hello(){
+  //  console.log(this);
+//}
+// debugger
+// const pet = {
+//     type: 'dog',
+//     sound: 'woof',
+//     makeSound(){
+//         return `The ${pet.type} make a ${this.sound}.`
+//     }
+// }
+// pet.makeSound();
+a = 1;
 function hello(){
-    console.log(this);
+   return this;
+}
+hello();
+
+const math = {
+    value1: 1,
+    value2: 2,
+    add(){
+        return this.value1 + this.value2
+    }
 }
