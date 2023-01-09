bede9fac106f215c75c072a72f88bc4f8f524888
gonzalomarcylabschool <gonzalo@marcylabschool.org>
2023-01-09 16:05:52 -0500

    updated the JS file for the ES class pt 1

--- unit-4/Lecture-7-ES6-classes/script.js
+++ unit-4/Lecture-7-ES6-classes/script.js
@@ -44,38 +44,41 @@ function makePerson(name, age, occupation){
   }
 }
 const gonzalo2 = makePerson('gonzalo', 34, 'teacher')
-const randy2 = makePerson('Randy', 19, 'Fellow')
+const randy2 = new Person('Randy', 19, 'Fellow')
 
 /* Make new Factory Function above this line */
 
 // Lets make a constructor together:
 /* Make new object below this line*/
-// function Person(name, age, occupation){
-//   this.name = name;
-//   this.age = age;
-//   this.occupation = occupation;
-//   this.friends = []
-// }
-// Person.prototype.hello = function (){
-//   return `Hello, I'm ${this.name}, at ${this.age} ${this.occupation}`
-// }
+function Person(name, age, occupation){
+  this.name = name;
+  this.age = age;
+  this.occupation = occupation;
+  this.friends = []
+}
+Person.prototype.hello = function (){
+  return `Hello, I'm ${this.name}, at ${this.age} ${this.occupation}`
+}
+Person.prototype.sayGoodBye= function(){
+  return `Good bye`;
+}
 
-const gonzalo3 = new Person('Gonzalo', 34, 'teacher')
+ const gonzalo3 = new Person('Gonzalo', 34, 'teacher')
 /* Make new object above this line */
 
 //Let's make a Person class and then a few instances!!
 
-class Person{
-  constructor(name, age, occupation){
-    this.name = name;
-    this.age = age;
-    this.occupation = occupation;
-    this.friends = []
-  }
-  hello(){
-  return `Hello, I'm ${this.name}, at ${this.age} ${this.occupation}`
-  }
-}
+// class Person{
+//   constructor(name, age, occupation){
+//     this.name = name;
+//     this.age = age;
+//     this.occupation = occupation;
+//     this.friends = []
+//   }
+//   hello(){
+//   return `Hello, I'm ${this.name}, at ${this.age} ${this.occupation}`
+//   }
+// }
 class Animal {
   constructor(name, legs, tail){
     this.name = name,
@@ -87,3 +90,33 @@ class Animal {
     console.log('Animal made Sound');
   }
 } 
+const viva = new Animal('viva', 4, true);
+
+class Rooms {
+  constructor(type, floors, sqFootage, windows, walls){
+    this.type = type;
+    this.floors = floors;
+    this.sqFootage = sqFootage;
+    this.windows = windows;
+    this.walls = walls;
+    this.furniture = []; 
+  }
+  turnOnLight(){
+    return `The light are now on!`
+  }
+  addFurniture(item) {
+      this.furniture.push(item);
+      return `You have added a ${item} to ${this.type}`
+    }
+}
+const bedRoom = new Rooms('bed room', 'wood', 100, true, 4);
+const kitchen = new Rooms('kitchen', 'tile', 120, true, 4);
+
+function HeadPhones(brand){
+  this.brand = brand;
+}
+HeadPhones.prototype.music = function() {
+  return`music is playing`
+}
+const beats = new HeadPhones('Beats');
+const airPods = new HeadPhones('Apple');
