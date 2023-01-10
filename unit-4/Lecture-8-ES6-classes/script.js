4343b8247e865c2804ac845cf61b8c36c0b90f35
gonzalomarcylabschool <gonzalo@marcylabschool.org>
2023-01-10 14:07:27 -0500

    updated the lecture notes

--- unit-4/Lecture-8-ES6-classes/script.js
+++ unit-4/Lecture-8-ES6-classes/script.js
@@ -1,4 +1,44 @@
 // write a class
+
+class Car{
+  constructor(make, model, year, color, mileage){
+    this.make = make;
+    this.model = model;
+    this.year = year;
+    this.color = color;
+    this.mileage = mileage;
+  }
+  drive(milesDriven){
+    if(typeof milesDriven != 'number'){
+      return `Invalid`
+    }else{
+      this.mileage += milesDriven;
+      return `The car is in motion.`
+    }
+    
+  }
+  turnLightsOn(){
+    return `The lights are now on!`
+  }
+}
+
+class CarType extends Car{
+  constructor(make, model, year, color, mileage, engineType){
+    super(make, model, year, color, mileage);
+    this.engineType = engineType;
+  }
+  turnLightsOn(){
+    return `Lights are On`
+  }
+}
+
+const gonzalosCar = new Car('Hyundai', 'Elantra', 2020, 'Silver', 19000)
+const car2 = new Car('Nissan', 'Altima', 2020, 'Grey', 9000)
+
+const car3 = new CarType('Hyundai', 'Elantra', 2024, 'Silver', 5, 'Electric')
+
+
+// instance vs class methods
 class Person {
   constructor(name, age, occupation){
       this.name = name;
@@ -8,7 +48,7 @@ class Person {
   }
   
   hello() { //this is the instance method
-    return `Hello, I'm ${this.name}, at ${this.age} ${occupation}` 
+    return `Hello, I'm ${this.name}, at ${this.age} ${this.occupation}` 
   }
   
   static onlyPerson() {//this is the class method
@@ -18,11 +58,11 @@ class Person {
 //
 const gonzalo = new Person("Gonzalo", 34, "Teacher")
 // What would it look like for us to make a new instance?
-
+const koumba = new Person('Koumba', 19, 'Fellow')
 // How do we invoke hello method?
-
-// Does it matter which instance I invoke the hello method on?
-
+gonzalo.hello()
+koumba.hello()
+// Does it matter which instance I invoke the hello method on?4, 5, 
 // What about the onlyPerson method?
 
 // Going back to the array example, this is what the array class under the hood would look like. 
@@ -44,12 +84,12 @@ class Array{
     //code
   }
 }
-
-const arr1 = new Array();
+const arr = [] 
+const arr1 = [1, 2, 3, 4];
 // What would it look like for us to make a new instance?
-
+const arr2 = [4, 5, 5];
 // How do we invoke push method?
-
+arr2.push(6);
 // Does it matter which instance I invoke the push method on?
 
 // What about the isArray method?
