4223fcb0f33766f36c6eda2e3593539480480b05
gonzalomarcylabschool <gonzalo@marcylabschool.org>
2023-01-11 10:45:01 -0500

    updated the lecture notes

--- unit-4/Lecture-9-Inheritance/script.js
+++ unit-4/Lecture-9-Inheritance/script.js
@@ -2,27 +2,31 @@ const gonzalo = {
   name: 'Gonzalo',
   age: 34,
   job: 'teacher'
-}
+};
 
+// Before ES6 Classes:
 function Book(title, author, year) {
   this.title = title;
   this.author = author;
   this.year = year;
-}
-    // getSummary
-Book.prototype. getSummary = function () {
-  return '${this.title} was written by ${this.author} in ${this.year}';
 };
-    // Magazine Constructor
+// 
+Book.prototype.getSummary = function () {
+  return `${this.title} was written by ${this.author} in ${this.year}`;
+};
+// Instantiate a Book object:
+const book1 = new Book('PapaBear', 'Gonzalo', 2022);
+
+  // Magazine constructor:
 function Magazine(title, author, year, month) {
   Book.call(this, title, author, year);
   this.month = month;
-}
-    // Inherit Prototype
-Magazine.prototype = Object.create(Book.prototype) ;
-    // Instantiate Magazine Object
-// const mag1 = new Magazine( 'Mag One', 'John Doe', '2018','Jan' );
-// console. log (mag1) ;
+};
+  // Inherit prototype from Book:
+Magazine.prototype = Object.create(Book.prototype);
+  // Instantiate a magazine object
+ const mag1 = new Magazine( 'Mag One', 'John Doe', '2018','Jan' );
+
 
 
   //Using ES6
@@ -31,15 +35,15 @@ Magazine.prototype = Object.create(Book.prototype) ;
 //     this.title = title;
 //     this.author = author;
 //     this.year = year;
-//   }
-//   getSummary = function () {
-//     return '${this.title} was written by ${this.author} in ${this.year}';
 //   };
-// }
+//   getSummary = function () {
+//     return `${this.title} was written by ${this.author} in ${this.year}`;
+//   };
+// };
 
 // class Magazine{//this is the subclass(child class)
 //   constructor(title, author, year, month) {
 //     super(title, author, year)
 //     this.month = month;
-//   }
-// }
+//   };
+// };
