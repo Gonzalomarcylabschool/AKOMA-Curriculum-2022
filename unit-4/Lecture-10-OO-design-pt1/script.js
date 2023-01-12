b4972bd3e03ce3314f441fc79edf7a50feb49bbb
gonzalomarcylabschool <gonzalo@marcylabschool.org>
2023-01-12 12:07:05 -0500

    Updated the notes and the files

--- unit-4/Lecture-10-OO-design-pt1/script.js
+++ unit-4/Lecture-10-OO-design-pt1/script.js
@@ -6,7 +6,12 @@
 4. `checkBalance` takes no parameters and returns a string in 
 the format `<owner> has $<balance> in this account.`
 5. `deposit` takes a single number parameters and adds it 
-to the `balance` property. It should also return a string in the format `"$<amount> was deposited to this account."`
-6. `withdraw` takes a single number parameters and subtracts it to the `balance` property. It should also return a string in the format `"$<amount> was withdrawn from this account."`
-7. **You should not be allowed to withdraw more than what's in the account**, in which case, return `"Not enough funds."` and do not update the `balance` property.
+to the `balance` property. It should also return a string in the format 
+`"$<amount> was deposited to this account."`
+6. `withdraw` takes a single number parameters and subtracts it to the 
+`balance` property. It should also return a string in the format 
+`"$<amount> was withdrawn from this account."`
+7. **You should not be allowed to withdraw 
+more than what's in the account**, in which case, return 
+`"Not enough funds."` and do not update the `balance` property.
 */
