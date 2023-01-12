5c14169a46242055c4b4e464dce3013ec326a9d0
gonzalomarcylabschool <gonzalo@marcylabschool.org>
2023-01-11 20:21:04 -0500

    updated some of the lecture notes

--- unit-4/Lecture-9-Inheritance/README.md
+++ unit-4/Lecture-9-Inheritance/README.md
@@ -14,26 +14,29 @@ console.log(Object.keys(gonzalo));
 
 ## Before ES6 classes
 
-In ES5, making your own object with inheritenace was a bit more complicated to achieve. You would need the following:
-* Constructor function
-* Prototype constructor for the methods
-* 
+In ES5, making your own object with inheritance was more complicated. 
+You would need the following:
+  - Constructor function
+  - Prototype constructor for the methods
+  - Object.call()
+  - Object.create(Object.prototype)
+
 
 ```JS
 // Parent constructor
 function Book(title, author, year) {
-this.title = title;
-this.author = author;
-this.year = year;
+  this.title = title;
+  this.author = author;
+  this.year = year;
 }
 
 Book.prototype.getSummary = function () {
-return `${this.title} was written by ${this.author} in ${this.year}`;
+  return `${this.title} was written by ${this.author} in ${this.year}`;
 };
 
 // Child constructor
 function Magazine(title, author, year, month) {
-Book.call(this, title, author, year);
+  Book.call(this, title, author, year);
 this.month = month;
 }
 // Inherit Prototype
@@ -44,28 +47,36 @@ const mag1 = new Magazine( 'Mag One', 'John Doe', '2018',
 console. log (mag 1) ;
 ```
 
-As always ES6 has come to the rescue and has made it easier to implement this. 
+As always, ES6 has come to the rescue and made implementing this easier. 
 
 To create a child class that inherits from a parent class, you can use the extends keyword and the super keyword to call the parent class' constructor and access its prototype object. For example:
 
 ```js
-class Book{// This is the superclass(parent class)
-    constructor(title, author, year) {
-        this.title = title;
-        this.author = author;
-        this.year = year;
-    };
-    getSummary = function () {
-        return `${this.title} was written by ${this.author} in ${this.year}`;
-    };
+class Book{// this is the superclass(parent class)
+  constructor(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+  };
+  getSummary = function () {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+  };
 };
 
-class Magazine{//this is the subclass(child class)
-    constructor(title, author, year, month) {
-        super(title, author, year);
-        this.month = month;
-    };
+class Magazine extends Book{//this is the subclass(child class)
+  constructor(title, author, year, month) {
+    super(title, author, year);
+    this.month = month;
+  };
 };
+
+//Let's make a comic class!
 ```
 
 ## Polymorphism 
+Polymorphism is the presentation of one interface for multiple data types.
+
+For example, integers, floats, and doubles are implicitly polymorphic: regardless of their different types, they can all be added, subtracted, multiplied, and so on.
+
+In the case of OOP, by making the class responsible for its code as well as its own data, polymorphism can be achieved in that each class has its own function that (once called) behaves properly for any object.
+
--- unit-4/Lecture-9-Inheritance/script.js
+++ unit-4/Lecture-9-Inheritance/script.js
@@ -1,49 +1,97 @@
+// Instances of objects can inherit all of the properties of Objects.
+
 const gonzalo = {
   name: 'Gonzalo',
   age: 34,
   job: 'teacher'
 };
+// gonzalo.toString(); // instance method
+// Object.keys(gonzalo); // class method
+ const jared = {
+  name: "Jared",
+  age: 20,
+  occupation: 'fellow'
+ }
+// jared.toString() //instance method
+// Object.keys(jared) //class method
+// We can see that both jared and gonzalo have inherited properties of the Object class, and both can use them.
 
 // Before ES6 Classes:
-function Book(title, author, year) {
-  this.title = title;
-  this.author = author;
-  this.year = year;
-};
-// 
-Book.prototype.getSummary = function () {
-  return `${this.title} was written by ${this.author} in ${this.year}`;
-};
+/*
+  In ES5, making your own object with inheritance was more complicated. 
+  You would need the following:
+    - Constructor function
+    - Prototype constructor for the methods
+    - Object.call()
+    - Object.create(Object.prototype)
+*/
+
+// function Book(title, author, year) {
+//   this.title = title;
+//   this.author = author;
+//   this.year = year;
+// };
+// // 
+// Book.prototype.getSummary = function () {
+//   return `${this.title} was written by ${this.author} in ${this.year}`;
+// };
 // Instantiate a Book object:
-const book1 = new Book('PapaBear', 'Gonzalo', 2022);
 
-  // Magazine constructor:
-function Magazine(title, author, year, month) {
-  Book.call(this, title, author, year);
-  this.month = month;
-};
+// Magazine constructor:
+// function Magazine(title, author, year, month) {
+//   Book.call(this, title, author, year);
+//   this.month = month;
+// };
   // Inherit prototype from Book:
-Magazine.prototype = Object.create(Book.prototype);
+// Magazine.prototype = Object.create(Book.prototype);
   // Instantiate a magazine object
- const mag1 = new Magazine( 'Mag One', 'John Doe', '2018','Jan' );
-
 
 
   //Using ES6
-// class Book{// This is the superclass(parent class)
-//   constructor(title, author, year) {
-//     this.title = title;
-//     this.author = author;
-//     this.year = year;
-//   };
-//   getSummary = function () {
-//     return `${this.title} was written by ${this.author} in ${this.year}`;
-//   };
-// };
+class Book{ // This is the superclass(parent class).
+  constructor(title, author, year) {
+    this.title = title;
+    this.author = author;
+    this.year = year;
+  };
+  getSummary() {
+    return `${this.title} was written by ${this.author} in ${this.year}`;
+  };
+};
 
-// class Magazine{//this is the subclass(child class)
-//   constructor(title, author, year, month) {
-//     super(title, author, year)
-//     this.month = month;
-//   };
-// };
+const book1 = new Book('PapaBear', 'Gonzalo', 2022);
+const book2 = new Book('Marcy Life', 'Isaac', 2023);
+
+class Magazine extends Book{// This is the subclass(child class).
+  constructor(title, author, year, month) {
+    super(title, author, year);
+    this.month = month;
+  };
+};
+
+const mag1 = new Magazine( 'Mag One', 'John Doe', '2018','Jan' );
+
+//Let's make a comic class.
+
+class Comic extends Book { // This is another the subclass(child class).
+  constructor(title, author, year, issue){
+    super(title, author, year);
+    this.issue = issue;
+  };
+  getSummary() {
+    return `${this.title}, issue ${this.issue}, was written by ${this.author} in ${this.year}`;
+  };
+  changeYear(newYear){ // This will change the state of the year property.
+    this.year = newYear;
+    return `The year has been changed to ${newYear}`
+  }
+};
+const superMan1 = new Comic('Super Man 1', 'Jerry Siegel', 1938,  1 )
+
+const literature = [mag1, book1, book2, superMan1]
+
+// With this code, we can see how mag1, book1, and book2 still use the original getSumary, and superMan1 uses the same method name but has a different result.
+
+// literature.forEach((e) => {
+//   console.log(e.getSummary());
+// });
