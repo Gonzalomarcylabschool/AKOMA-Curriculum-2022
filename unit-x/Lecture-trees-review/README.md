b14b3b1b704bf5862c31e7f94831e1ee091a904a
gonzalomarcylabschool <gonzalo@marcylabschool.org>
2023-02-08 10:58:33 -0500

    update

--- unit-x/Lecture-trees-review/README.md
+++ unit-x/Lecture-trees-review/README.md
@@ -1,2 +1,174 @@
 # Trees pt 3 / review
 
+## isUnivalTree
+
+```js
+function isUnivalTree(node){
+  //check to see if the node that is passed in does not have children which then makes it UniValue. This is our base case.
+
+  //Declare a variable the will hold a boolean if the left match the root node.
+  //Declare a variable the will hold a boolean if the right match the root node.
+
+  //Check to see if there is a left node
+     //store in the variable the boolean by evaluating if the root is equal to the left node
+  
+  //Check to see if there is a right node
+     //store in the variable the boolean by evaluating if the root is equal to the right node
+  //if there is no left node
+    //return the variable that hold the boolean for the right and invoke isUnivalTree on the right node
+  
+   //if there is no right node
+    //return the variable that hold the boolean for the left and invoke isUnivalTree on the left node
+
+  //return the variable that hold the boolean for the left and the right and invoke isUnivalTree on the left and on the right
+}
+```
+
+<details><summary>Solution</summary>
+
+```js
+function isUnivalTree(node){
+  if (!node.left && !node.right) {
+    return true;
+  }
+
+  let leftMatches;
+  let rightMatches;
+  if (node.left) {
+    leftMatches = node.value === node.left.value;
+  }
+  if (node.right) {
+    rightMatches = node.value === node.right.value;
+  }
+  if (!node.left) {
+    return rightMatches && isUnivalTree(node.right);
+  }
+  if (!node.right) {
+    return leftMatches && isUnivalTree(node.left);
+  }
+
+  return leftMatches && rightMatches && isUnivalTree(node.left) && isUnivalTree(node.right)
+}
+```
+
+</details>
+
+## isUnivalTree refactor
+
+```js
+//refactor
+function isUnivalTree(node){
+  //return the helper function which take in the node and the value of the node
+}
+
+function isUnivalTreeHelper(node, value) { //declare the helper function that take in the node and the value of the node.
+  // if the node is null
+     //return true this is the base case
+  
+   // if the node value is not equal to the value
+    //return false
+  
+  //check the left node and the right node recursively
+}
+```
+
+<details><summary>Solution</summary>
+
+```js
+function isUnivalTree(node){
+  return isUnivalTreeHelper(node, node.value);
+}
+
+function isUnivalTreeHelper(node, value) {
+  if (!node) {
+    return true;
+  }
+  if (node.value !== value) {
+    return false;
+  }
+  return isUnivalTreeHelper(node.left, value) && isUnivalTreeHelper(node.right, value);
+}
+```
+
+</details>
+
+
+## invert
+
+```js
+function invert(node){ 
+  // check to see the left and right node are not null // this is the base case
+    // declare a variable to store to the left
+    // store the value returned by invoking function with the right node into the left node
+    // store the value returned by invoking function with the left node into the right node
+  
+  //return the node
+}
+```
+
+<details><summary>Solution</summary>
+
+```js
+if (node.left && node.right){ //
+    let left = node.left;
+    node.left = invert(node.right);
+    node.right = invert(temp);
+  }
+  return node;
+```
+
+</details>
+
+## secondMinimumNode
+
+```js
+function secondMinimumNode(node){
+  //base case: if the node is null or  there is no left and right node
+    //return -1
+  
+
+  // store the left value of the node
+  //store the right value of the node
+  //if the left value is === to the node value
+    // invoke the function on the left node and store the value that is returned in to the 'left' variable
+  
+  //if the left value is === to the node value
+    //invoke the function on the right node and store the value that is returned in to the 'right' variable
+  
+  //if the left and right variable are not -1 
+    //return the low est value (what method can we use here)
+  //check to see if the 'left' is not -1
+    // return left 
+   //else 
+    //return right;
+  
+}
+```
+
+<details><summary>Solution</summary>
+
+```js
+function secondMinimumNode(node){
+  if (!node || (!node.left && !node.right )) {
+    return -1;
+  }
+
+  let left = node.left.value;
+  let right = node.right.value;
+  if (left === node.value) {
+    left = findSecondMinimumValue(node.left);
+  }
+  if (right === node.value) {
+    right = findSecondMinimumValue(node.right);
+  }
+  if (left !== -1 && right !== -1) {
+    return Math.min(left, right);
+  } else if (left !== -1) {
+    return left;
+  } else {
+    return right;
+  }
+}
+```
+
+</details>
---
+++ unit-x/Lecture-trees-review/script.js
@@ -0,0 +1,131 @@
+class BinarySearchTree{
+  constructor(value = null) {
+    this.value = value;
+    this.left = null;
+    this.right = null;
+  }
+
+  insert(newValue) {
+    const newSubtree = new BinarySearchTree(newValue)
+    if (!this.value) { // looked back and this is the best solution
+      this.value = newSubtree;
+      return this;
+    } 
+
+    let currentNode = this // Declare a variable that will be the pointer to the node that we are in.
+    while (true) {
+      if (newValue === currentNode.value) {//Check to see if the value is === to the value of the current node
+        return this;
+      }
+      if(newValue < currentNode.value) { // Check to see if the value is less than the value of the current node
+        if(!currentNode.left) {// Check to see if there is a value to the left
+          currentNode.left = newSubtree;// Add the new node here
+          return this;
+        }
+        currentNode = currentNode.left; // Move on to the next node
+      } else { //if the value is greater than the value of the current node
+          if(!currentNode.right) { //Check to see if there is a value to the
+            currentNode.right = newSubtree; //Add the new node here
+            return this;
+          } else {
+          currentNode = currentNode.right;// // Move on to the next node
+        }
+      }
+    }
+  }
+  find(target){
+    if (!this.value){
+      false
+    }
+    let currentNode = this;
+    let found = false;
+    while(currentNode && !found) {
+      if(target < currentNode.value){
+        currentNode = currentNode.left;
+      } else if (target > currentNode.value) {
+        currentNode = currentNode.right;
+      } else {
+        return currentNode
+      }
+    }
+    return undefined;
+  }
+}
+
+const bst = new BinarySearchTree(55);
+
+bst.insert(25);
+bst.insert(300);
+bst.insert(200);
+bst.insert(75);
+bst.insert(30);
+bst.insert(15);
+bst.insert(24);
+bst.insert(325);
+
+function isUnivalTree(node){
+  //check to see if the node that is passed in does not have children which then makes it UniValue. This is our base case.
+
+  //Declare a variable the will hold a boolean if the left match the root node.
+  //Declare a variable the will hold a boolean if the right match the root node.
+
+  //Check to see if there is a left node
+     //store in the variable the boolean by evaluating if the root is equal to the left node
+  
+  //Check to see if there is a right node
+     //store in the variable the boolean by evaluating if the root is equal to the right node
+  //if there is no left node
+    //return the variable that hold the boolean for the right and invoke isUnivalTree on the right node
+  
+   //if there is no right node
+    //return the variable that hold the boolean for the left and invoke isUnivalTree on the left node
+
+  //return the variable that hold the boolean for the left and the right and invoke isUnivalTree on the left and on the right
+}
+
+//refactor
+function isUnivalTree(node){
+  //return the helper function which take in the node and the value of the node
+}
+
+function isUnivalTreeHelper(node, value) { //declare the helper function that take in the node and the value of the node.
+  // if the node is null
+     //return true this is the base case
+  
+   // if the node value is not equal to the value
+    //return false
+  
+  //check the left node and the right node recursively
+}
+
+function invert(node){ 
+  // check to see the left and right node are not null // this is the base case
+    // declare a variable to store to the left
+    // store the value returned by invoking function with the right node into the left node
+    // store the value returned by invoking function with the left node into the right node
+  
+  //return the node
+}
+
+
+function secondMinimumNode(node){
+  //base case: if the node is null or  there is no left and right node
+    //return -1
+  
+
+  // store the left value of the node
+  //store the right value of the node
+  //if the left value is === to the node value
+    // invoke the function on the left node and store the value that is returned in to the 'left' variable
+  
+  //if the left value is === to the node value
+    //invoke the function on the right node and store the value that is returned in to the 'right' variable
+  
+  //if the left and right variable are not -1 
+    //return the low est value (what method can we use here)
+  //check to see if the 'left' is not -1
+    // return left 
+   //else 
+    //return right;
+  
+}
