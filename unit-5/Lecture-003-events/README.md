efdfd8fbdecbab97176a413a6b2fab121ed05e59
gonzalomarcylabschool <gonzalo@marcylabschool.org>
2023-02-14 14:06:38 -0500

    update

--- unit-5/Lecture-003-events/README.md
+++ unit-5/Lecture-003-events/README.md
@@ -15,17 +15,21 @@ click => things happens
 press => things happens
 load => thing happens
 
+when I press `w` my charter walks forward. 
+
 There is a few ways we can make this happen
 
 ```HTML
-<element onclick="myScript">
+<element type="callback">
+<h1 onclick="callback"></h1>
 ```
 ```js
-element.onclick = function(){myScript};
-
+element.type = function(){callback};
+document.querySelector('h1').onclick = function(){callback};
 // or
 
-element.addEventListener("click", myScript)
+element.addEventListener("type", callback)
+document.querySelector('h1').addEventListener("click", callback)
 ```
 
 ## DOMContentLoaded
@@ -37,6 +41,8 @@ Let load our web page and see whats going on under the hood.
 <script src="script.js"></script>
 
 console.log('test');
+
+document.body? // is it there?
 ```
 
 Now what happens if we load this into our `<body>`?
@@ -44,11 +50,79 @@ Now what happens if we load this into our `<body>`?
 So far everything still kind seems the same?
 
 Let's put it back in the `<head>` and throw a debugger in here.
+```js
+document;
+document.body;
+document.head;
+```
+
+What is happening is that the browser loads the page from top to bottom. 
+
+```HTML
+<!DOCTYPE html>
+<!--^loaded-->
+<html lang="en">
+  <!--^loaded-->
+<head>
+  <!--^loaded-->
+  <meta charset="UTF-8">
+  <!--^loaded-->
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <!--^loaded-->
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!--^loaded-->
+  <title>Document</title>
+  <!--^loaded-->
+  <script src="script.js"></script> <!--load everything from here-->
+  <!--^loaded-->
+</head>
+<!--^loaded-->
+<body>
+  <!--^loaded-->
+```
+So when we put the `<script>` in the head `<head>` and load the page with a debugger, the `<body>` is not loaded yet. 
+
+Let's try this again with the script in the body and see what happens `
+
+What you might notice is that the the `<body>` is now loaded.
+
+So what is the best practice? We are going to use `DOMContentedLoaded`. If we ever have any dependance or have code that depends on elements this is best practice. 
+
+```js
+document.addEvents('DOMContentLoaded', () => {
+  //function code  
+});
+```
 
 
+## Lets try this out:
 
-## Event listener / Event handler
+```js
+const friendsList = ['Laura', 'Ben', 'Itzel', 'Ann', 'Maya', 'Ruben']
 
-click => things happens
-press => things happens
 
+friendsList.forEach(f => addFriend(f))
+
+
+function addFriend(friendName){
+  let li = document.createElement("li");
+  li.textContent = friendName;
+  document.querySelector('#list').appendChild(li);
+}
+```
+
+## Click events and more
+So I have some code that I want to count how many times I click on a button. 
+
+Let's see how we can make them work: 
+
+```js
+let countOne = 0;
+const clickCount1 = document.getElementById('counterOne');
+document.getElementById("myBtn1").addEventListener("click", buttonClick);
+
+function buttonClick(){
+  countOne++
+  clickCount1.innerText = countOne;
+}
+```
--- unit-5/Lecture-003-events/script.js
+++ unit-5/Lecture-003-events/script.js
@@ -1,65 +1,103 @@
-console.log('test');
+// debugger;
+// console.log(document.querySelector('body'));
+
+// document.addEventListener('DOMContentLoaded', () => {//will listen to when the DOM is loaded
+//   // debugger
+//   console.log(document.querySelector('body'));
+// })
+// callBack(print);
+
+// function callBack(func){
+//   debugger;
+//   func();
+// }
+
+// function print(){
+//   console.log('test')
+// }
+document.addEventListener('DOMContentLoaded', () => {//will listen to when the DOM is loaded
+  //   // debugger
+  //   console.log(document.querySelector('body'));
+  const friendsList = ['Laura', 'Ben', 'Itzel', 'Ann', 'Maya', 'Ruben']
+
+
+  friendsList.forEach(f => addFriend(f))
+
+
+  // function addFriend(friendName){
+  //   console.log(friendName);
+  //   let li = document.createElement("li");
+  //   li.textContent = friendName;
+  //   document.querySelector('#list').appendChild(li);
+  // }
+
+  // let countOne = 0;
+  // const clickCount1 = document.getElementById('counterOne');
+  // document.getElementById("myBtn1").addEventListener("click", buttonClick);
+
+  // function buttonClick(){
+  //   countOne++
+  //   clickCount1.innerText = countOne;
+  // }
+})
 
 
 
 
 
 
+//debugger;
 
 
 
+document.addEventListener('DOMContentLoaded', (event) => {
 
-
-// document.addEventListener('DOMContentLoaded', (event) => {
-
-//   let countOne = 0;
-//   let countTwo = 0;
-//   let countThree = 0;
-//   const clickCount1 = document.getElementById('counterOne');
-//   const clickCount2 = document.getElementById('counterTwo');
-//   const clickCount3 = document.getElementById('counterThree');
-//   document.getElementById("myBtn1").addEventListener("click", function(){buttonClick(clickCount1)});
-//   document.getElementById("myBtn2").addEventListener("click", function(){buttonClick(clickCount2)});
-//   document.getElementById("myBtn3").addEventListener("click", function(){buttonClick(clickCount3)});
+  let countOne = 0;
+  let countTwo = 0;
+  let countThree = 0;
+  const clickCount1 = document.getElementById('counterOne');
+  const clickCount2 = document.getElementById('counterTwo');
+  const clickCount3 = document.getElementById('counterThree');
+  document.getElementById("myBtn1").addEventListener("click", function(){buttonClick(clickCount1)});
+  document.getElementById("myBtn2").addEventListener("click", function(){buttonClick(clickCount2)});
+  document.getElementById("myBtn3").addEventListener("click", function(){buttonClick(clickCount3)});
   
-//   function buttonClick(button){
-//     let count = 0;
-//     if(button === clickCount1){
-//       countOne++
-//       count = countOne;
-//     } else if(button === clickCount2) {
-//       countTwo++
-//       count = countTwo;
-//     } else {
-//       countThree++
-//       count = countThree;
-//     }
-//     button.innerText = count
-//   }
+  function buttonClick(button){
+    let count = 0;
+    if(button === clickCount1){
+      countOne++
+      count = countOne;
+    } else if(button === clickCount2) {
+      countTwo++
+      count = countTwo;
+    } else {
+      countThree++
+      count = countThree;
+    }
+    button.textContent = count
+  }
   
   
   // document.getElementById('submit').addEventListener('click', comment);
-  
-  // function comment(e){
-  //   e.preventDefault();
-  //   const input = document.querySelector('#pageComment');
-  //   const p = document.createElement("p");
-  //   p.textContent = input.value
-  //   document.getElementById('comment').appendChild(p);
-  //   input.value = ''
+  document.getElementById('submit').onclick = comment();
 
-  // }
+  function comment(e){
+    e.preventDefault();
+    const input = document.querySelector('#pageComment');
+    const p = document.createElement("p");
+    p.textContent = input.value
+    document.getElementById('comment').appendChild(p);
+    input.value = ''
 
-  // let colorCount = 0;
-  // const colors = ['blue', 'green', 'coral', 'red', 'pink']
-  // document.getElementById('pageComment').oninput = function(){
-  //   // debugger;
-  //   colorCount++
-  //   document.getElementById('submit').style.backgroundColor = colors[colorCount % 5]
-  // }
+  }
 
-  
-
-// });
+  let colorCount = 0;
+  const colors = ['blue', 'green', 'coral', 'red', 'pink']
+  document.getElementById('pageComment').oninput = function(){
+    // debugger;
+    colorCount++
+    document.getElementById('submit').style.backgroundColor = colors[colorCount % 5]
+  }
+});
 
 
