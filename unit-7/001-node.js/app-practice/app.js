8c7ae9d357483b91577d26907a3908e3292c66e6
gonzalomarcylabschool <gonzalo@marcylabschool.org>
2023-04-04 14:56:00 -0400

    update

---
+++ unit-7/001-node.js/app-practice/app.js
@@ -0,0 +1,12 @@
+// const supervillains = require('supervillains');
+// const randomHero = supervillains.random();
+// console.log(randomHero);
+
+// const utils = require('./utils')
+
+
+const { INSTRUCTOR_NAME,  rollDie } = require("./utils"); 
+
+console.log(rollDie())
+console.log(INSTRUCTOR_NAME)
+
---
+++ unit-7/001-node.js/app-practice/utils.js
@@ -0,0 +1,9 @@
+const rollDie = () => {
+  return Math.ceil(Math.random() * 6);
+}
+const INSTRUCTOR_NAME = "GONZALO";
+
+module.exports = {
+  rollDie,
+  INSTRUCTOR_NAME
+} 
